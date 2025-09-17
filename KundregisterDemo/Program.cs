55324b9c3fd17fb140f971f23ea62816be42ef6d
Marcus Ackre Medina <40459915+MarcusMedina@users.noreply.github.com>
2025-09-17 14:25:51 +0200

    Welcome del fixad

---
+++ KundregisterDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor= ConsoleColor.White;
+
+Console.Clear();
+Console.WriteLine("Hello, Kundregister 🐐!");
+
+
+
