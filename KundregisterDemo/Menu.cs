77dc35815386ce92e2f5cb2c610f737141948c05
Marcus Ackre Medina <40459915+MarcusMedina@users.noreply.github.com>
2025-09-17 14:38:09 +0200

    add: Menu display

---
+++ KundregisterDemo/Menu.cs
@@ -0,0 +1,19 @@
+﻿internal class Menu
+{
+    public Menu()
+    {
+        
+    }
+
+    public void ShowMenu()
+    {
+        // CRUD = Create, Read, Update, Delete
+        Console.WriteLine("1 - List customers"); // Read
+        Console.WriteLine("2 - Add customer");   // Create
+        Console.WriteLine("3 - Edit customer");  // Update
+        Console.WriteLine("4 - Delete Customer");// Delete
+        Console.WriteLine("5 - quit");
+
+
+    }
+}
